0b46d4eecb43d3afd934c72cf80436d9bf132fb7
RazibHossain <razib@divine-it.net>
2024-05-02 08:24:58 +0600

    webclient configured

--- Online Shopping Application Using Spring MicroServices/shopping-micro/order-service/src/main/java/com/razib/orderservice/controllers/HelloController.java
+++ Online Shopping Application Using Spring MicroServices/shopping-micro/order-service/src/main/java/com/razib/orderservice/controllers/HelloController.java
@@ -1,5 +1,7 @@
 package com.razib.orderservice.controllers;
 
+import com.razib.orderservice.services.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/orderService")
 public class HelloController {
 
+//    @Autowired
+//    HelloService helloService;
     @RequestMapping(value = "/hello",method = RequestMethod.GET)
     public Object hello(){
+
+//        Object obj = helloService.communicateWithInventoryService();
+
         return "hello";
     }
 }
---
+++ Online Shopping Application Using Spring MicroServices/shopping-micro/order-service/src/main/java/com/razib/orderservice/services/HelloService.java
@@ -0,0 +1,21 @@
+package com.razib.orderservice.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Service
+//@RequiredArgsConstructor
+public class HelloService {
+//    private final WebClient webClient;
+//    public Object communicateWithInventoryService() {
+//        Object obj = webClient.get()
+//                .uri("http://localhost:7777/api/inventoryService/hello?ajax")
+//                .retrieve()
+//                .bodyToMono(Object.class)
+//                .block();
+//
+//        return obj;
+//    }
+}
